863c6c32cf27eb72cdf6b1f8558ae6b7468f44b0
parsa_j42 <parsajfri42@gmail.com>
2025-04-20 20:05:21 -0600

    Fix landing (loggedout) header

--- src/components/pages/LandingPage/components/LoggedOut.tsx
+++ src/components/pages/LandingPage/components/LoggedOut.tsx
@@ -1,13 +1,15 @@
-import {Box, Stack, Title, Text, useMantineTheme, Image, Group, Flex, Divider} from '@mantine/core';
+import {Box, Button, Divider, Flex, Group, Image, Stack, Text, Title, useMantineTheme} from '@mantine/core';
 import WavyBackground from '@components/shared/WavyBackground/WavyBackground.tsx';
 import ProjectListContainer from "@components/shared/ProjectList/ProjectList.tsx";
 import RoundedButton from "@components/shared/RoundedButton/RoundedButton.tsx";
+import {useNavigate} from "react-router-dom";
 
 // SVG paths for wave effects
 const WAVE_PATH = "M 0 43 C 50 52 50 52 100 43 L 100 -30 L 0 -30 Z";
 
 export function LoggedOut() {
     const theme = useMantineTheme();
+    const navigate = useNavigate();
 
     // Wave dimension configurations
     const waveHeight = 1300;
@@ -30,19 +32,25 @@ export function LoggedOut() {
                 extraBottomPadding="0px"
             >
 
-            <Box pt="50px" pb="150px">
-                        <Group justify="center" align="top" gap="60px">
+                <Box pt="50px" pb="150px">
+                    <Group justify="center" align="top" gap="60px">
                         <Stack align="flex-start" gap="lg">
                             <Title order={1} ta="left" size="48px" fw={400} lh={1.2}>
-                                Make your great ideas <br/> a reality
+                                Turn your ideas <br/>into reality. <br/>Build. Collaborate. <br/>Launch.
                             </Title>
                             <Text size="xl" ta="left" c="dimmed" lh={1.4}>
                                 Post your project ideas and find the <br/> best‑fit team members.
                             </Text>
+                            <Group>
+                                <Button variant="filled" color="mainBlue.6"
+                                        onClick={() => navigate("/SignUp")}>Create Project</Button>
+                                <Button variant="filled" color="mainBlue.6"
+                                        onClick={() => navigate("/Discover")}>Find a Project</Button>
+                            </Group>
                         </Stack>
-                        <Image src="/placeholderImageHorizontal.svg" h={450} w="auto"/>
-                        </Group>
-                    </Box>
+                        <Image src="/landing_image.png" h={450} w="auto"/>
+                    </Group>
+                </Box>
 
                 <Box
                     w="70%"
@@ -97,10 +105,13 @@ export function LoggedOut() {
 
                             </Box>
                             <Box ml="10%">
-                            <RoundedButton w="50%" color="mainRed.6" variant="filled" fw="500">Sign Up</RoundedButton>
-                                <Text c="mainRed.6" fw="700" ml="1%" size="xs" mt="md" style={{ textAlign: 'left' }}>
-                                    By clicking Sign Up you<span style={{fontFamily: 'calibri'}}>'</span>re confirming that you agree with our&nbsp;
-                                    <Text c="black" component="a" href="/terms" size="xs" td="underline">Terms and Conditions</Text>
+                                <RoundedButton w="50%" color="mainRed.6" variant="filled" fw="500">Sign
+                                    Up</RoundedButton>
+                                <Text c="mainRed.6" fw="700" ml="1%" size="xs" mt="md" style={{textAlign: 'left'}}>
+                                    By clicking Sign Up you<span style={{fontFamily: 'calibri'}}>'</span>re confirming
+                                    that you agree with our&nbsp;
+                                    <Text c="black" component="a" href="/terms" size="xs" td="underline">Terms and
+                                        Conditions</Text>
                                 </Text>
 
                             </Box>
@@ -114,12 +125,12 @@ export function LoggedOut() {
             </WavyBackground>
 
             {/* New Projects Section */}
-                <Box mb="50px" mt="md">
-                    <Title order={2} ta="center" mt="xl" mb="50px" size="36px" fw={400}>
-                        New Projects on The Board
-                    </Title>
-                    <ProjectListContainer />
-                </Box>
+            <Box mb="50px" mt="md">
+                <Title order={2} ta="center" mt="xl" mb="50px" size="36px" fw={400}>
+                    New Projects on The Board
+                </Title>
+                <ProjectListContainer/>
+            </Box>
         </Stack>
     );
 }
